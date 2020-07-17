917e51875cf61e3e20042fff5e7b2db7c6722a93
Coffee-Mac <yxlimo@gmail.com>
2020-07-17 23:31:30 +0800

    p0035: 二分查找

---
+++ problem/0035_search_insert_position/main.go
@@ -0,0 +1,17 @@
+package p0035
+
+func searchInsert(nums []int, target int) int {
+	start, end := 0, len(nums)-1
+	for start <= end {
+		mid := (end + start) / 2
+		if nums[mid] == target {
+			return mid
+		}
+		if nums[mid] > target {
+			end = mid - 1
+		} else {
+			start = mid + 1
+		}
+	}
+	return start
+}
---
+++ problem/0035_search_insert_position/main_test.go
@@ -0,0 +1,55 @@
+package p0035
+
+import "testing"
+
+func Test_searchInsert(t *testing.T) {
+	type args struct {
+		nums   []int
+		target int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			name: "target 5",
+			args: args{
+				nums:   []int{1, 3, 5, 6},
+				target: 5,
+			},
+			want: 2,
+		},
+		{
+			name: "target 2",
+			args: args{
+				nums:   []int{1, 3, 5, 6},
+				target: 2,
+			},
+			want: 1,
+		},
+		{
+			name: "target 7",
+			args: args{
+				nums:   []int{1, 3, 5, 6},
+				target: 7,
+			},
+			want: 4,
+		},
+		{
+			name: "target 0",
+			args: args{
+				nums:   []int{1, 3, 5, 6},
+				target: 0,
+			},
+			want: 0,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := searchInsert(tt.args.nums, tt.args.target); got != tt.want {
+				t.Errorf("searchInsert() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
